30a3e425cfbb2ada2fd3c2bb3cc62eeda6285d22
Cristian Daniel Gomez Escobar <3046751560115@ingenieria.usac.edu.gt>
2022-08-22 00:50:21 -0600

    se añadieron nuevos atributos

--- NodoCelda.py
+++ NodoCelda.py
@@ -1,7 +1,8 @@
 class Celda:
-    def __init__(self, fila,columna,tipo):
+    def __init__(self, fila=None,columna=None,tipo=None):
         self.fila=fila
         self.columna=columna
         self.tipo=tipo
         self.siguiente=None
+        self.anterior=None
         
