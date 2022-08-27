80b3c033bc0b71d4a18f24835dc2267b75eb6d46
Cris1928 <98928867+Cris1928@users.noreply.github.com>
2022-08-27 02:55:31 -0600

    Delete NodoCelda.py

--- NodoCelda.py
+++
@@ -1,8 +0,0 @@
-class Celda:
-    def __init__(self, fila=None,columna=None,tipo=None):
-        self.fila=fila
-        self.columna=columna
-        self.tipo=tipo
-        self.siguiente=None
-        self.anterior=None
-        
