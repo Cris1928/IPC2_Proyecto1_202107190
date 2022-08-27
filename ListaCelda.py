888c2bc5df8eee4fc558ec3768addd35755130d7
Cristian Daniel Gomez Escobar <3046751560115@ingenieria.usac.edu.gt>
2022-08-27 03:23:58 -0600

    actualizacion de codigo

--- ListaCelda.py
+++ ListaCelda.py
@@ -21,9 +21,194 @@ class ListaC:
             nuevasceldas.anterior=self.ultimo
             self.ultimo=nuevasceldas
 
-  
+
+    def evaluar(self):
+        actual=self.primero
+        while actual is not None:
+            if actual.tipo==0:
+                filarriba=(int(actual.fila) +1)
+                columnaderecha=(int(actual.columna)+1)
+                filabajo=(int(actual.fila)-1)
+                columnaizquierda=(int(actual.columna)-1)
+                filanactual=(int(actual.fila))
+                columnanactual=(int(actual.columna))
+                actual2=self.primero
+                cumple=0
+                #evalua cuales se contagian 
+                while actual2 is not None:
+
+                    if actual2.fila== filarriba and actual2.columna==columnaizquierda:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    elif actual2.fila== filarriba and actual2.columna==columnanactual:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    elif actual2.fila==filarriba and actual2.columna==columnaderecha:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    elif actual2.fila==filanactual and actual2.columna==columnaizquierda:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    elif actual2.fila==filanactual and actual2.columna==columnaderecha:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    elif actual2.fila==filabajo and actual2.columna==columnaizquierda:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    elif actual2.fila==filabajo and actual2.columna==columnanactual:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    elif actual2.fila==filabajo and actual2.columna==columnaderecha:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                    else:
+                        pass
+                    
+                    if cumple == 3:
+                        actual.tipo=3
+                    else:
+                        actual.tipo=0
+                    actual2=actual2.siguiente
+
+                #evalua cuales sanaran
+            elif actual.tipo==1:
+                filarriba=(int(actual.fila) +1)
+                columnaderecha=(int(actual.columna)+1)
+                filabajo=(int(actual.fila)-1)
+                columnaizquierda=(int(actual.columna)-1)
+                filanactual=(int(actual.fila))
+                columnanactual=(int(actual.columna))
+                actual2=self.primero
+                cumple=0
+                while actual2 is not None:
+
+                    if actual2.fila== filarriba and actual2.columna==columnaizquierda:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                        
+                    elif actual2.fila== filarriba and actual2.columna==columnanactual:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                      
+                    elif actual2.fila==filarriba and actual2.columna==columnaderecha:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                       
+                    elif actual2.fila==filanactual and actual2.columna==columnaizquierda:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                       
+                    elif actual2.fila==filanactual and actual2.columna==columnaderecha:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                        
+                    elif actual2.fila==filabajo and actual2.columna==columnaizquierda:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                       
+                    elif actual2.fila==filabajo and actual2.columna==columnanactual:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                       
+                    elif actual2.fila==filabajo and actual2.columna==columnaderecha:
+                        if actual2.tipo==1:
+                            cumple=cumple+1
+                        elif actual2.tipo==2:
+                            cumple=cumple+1
+                        else:
+                            pass
+                       
+                    else:
+                        pass
+                    
+               
+                    if cumple ==2:
+                        actual.tipo=1
+                    elif cumple ==3:
+                        actual.tipo=1
+                    else:
+                        actual.tipo=2
+
+                    actual2=actual2.siguiente
+            actual=actual.siguiente
+
+        actual=self.primero       
+        while actual is not None:
+            if actual.tipo==2:
+                actual.tipo=0
+            elif actual.tipo==3:
+                actual.tipo=1
+            else:
+                pass
+            actual=actual.siguiente
 
 
+
+
+
+
+
+    
 
     def insertar(self,fila,columna,tipo):
         nuevo=Celda(fila,columna,tipo)
