80aa4c49d30aed625748c5401fa65589d1f56852
Cris1928 <98928867+Cris1928@users.noreply.github.com>
2022-09-06 01:13:05 -0600

    Add files via upload

---
+++ NodoPaciente.py
@@ -0,0 +1,9 @@
+from ListaCelda import ListaC
+class pacienten:
+    def __init__(self, nombre=None,edad=None,periodo=None,m=None):
+        self.nombre=nombre
+        self.edad=edad
+        self.periodo=periodo
+        self.m=m
+        self.celdas=ListaC()
+        self.siguiente=None
