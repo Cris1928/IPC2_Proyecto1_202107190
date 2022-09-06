5274ae37ad6ca6f77a1e9ddb7c309d108465d56e
Cris1928 <98928867+Cris1928@users.noreply.github.com>
2022-09-06 01:21:07 -0600

    Add files via upload

---
+++ NodoPasiente.py
@@ -0,0 +1,8 @@
+class nodopasiente():
+    def __init__(self, nombre, edad,periodos,m,celda):
+        self.nombre=nombre
+        self.edad=edad
+        self.periodos=periodos
+        self.m=m
+        self.celda=celda
+        self.siguiente=None
