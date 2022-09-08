34ef58feb41821cf858664d19ae97aac26fd073e
Cris1928 <98928867+Cris1928@users.noreply.github.com>
2022-09-07 22:58:58 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ from NodoPaciente import pacienten
 from ListarPasiente import passiente
 from ListaNumeral import ListadoNumeral
 from fpdf import FPDF
+from xml.dom import minidom
 import os
-
-#C:\Users\USER\Desktop\Prueba1.xml
-#C:\Users\USER\Desktop\IPC2_Proyecto1_202107190\prueba.xml
-
+from colorama import Fore
 def cargarXML(root):
 
     for elemento in root:
@@ -21,29 +19,29 @@ def XML(root):
     pass
 
 def menu():
+    global numeroxml
+    numeroxml=1
     global nuevonumero
     nuevonumero=ListadoNumeral()
     while True:
-        print('|************************* Menú ************************* |')
-        print(" ")
-        print(" → 1. Cargar archivo XML.")
-        print(" → 2. Diagnosticar paciente")
-        print(" → 3. Generar reporte de diagnostico de los pacientes")
-        print(' → 4. Salir.')
-        print(" ")
-        print('|******************************************************* |')
-            
+        print(Fore.LIGHTYELLOW_EX+'*************************** Menú **************************')
+        print("*                                                         *")
+        print("* → 1. Cargar archivo XML.                                *")
+        print("* → 2. Diagnosticar paciente.                             *")
+        print("* → 3. Evaluar si posee alguna enfermedad-                *")
+        print("* → 4. Generar reporte de diagnostico de los pacientes.   *")
+        print('* → 5. Salir.                                             *')
+        print("*                                                         *")
+        print('***********************************************************')
         print()
-        n = input('Digite la opción a realizar: ')
+        n = input(Fore.CYAN+'Digite la opción a realizar: ')
         print()
         if n == "1":
 #C:\Users\USER\Desktop\Prueba1.xml
 #C:\Users\USER\Desktop\IPC2_Proyecto1_202107190\prueba.xml
             pacientep=passiente()
             pacientep.limpiarList()
-        #  tree=ET.parse("./prueba.xml")
-        # root=tree.getroot()
-    # if n=="2":
+ 
             ruta = input('ingrese la dirección de su archivo XML: ')
         elif n=="2":
             nuevonumero=ListadoNumeral()
@@ -57,16 +55,12 @@ def menu():
                         for dato in datospersonales:
                             if dato.tag.lower() =="nombre":
                                 namep=str(dato.text)
-                            # print("nombre: ",dato.text)
                             if dato.tag.lower() =="edad":
                                 edadp=str(dato.text)
-                            # print("edad: ",dato.text)
 
                     if datospersonales.tag.lower() =="periodos":
                         periodosp=str(datospersonales.text)
-                    # print("periodos: ",datospersonales.text)
                     if datospersonales.tag.lower() =="m":
-                        #print("m: ",datospersonales.text)
                         m=int(datospersonales.text)+1
                         mp=str(datospersonales.text)
                         neww=pacienten(namep,edadp,periodosp,mp)
@@ -85,25 +79,11 @@ def menu():
                             j=1
                             for y in  range(j, m):
                                 npaciente.celdas.buscar(int(x),int(y))
-            
-                        #  pass
-                            
-                #           print("fila: ",rejilla.attrib["f"])
-                #          print("columna: ",rejilla.attrib["c"])
-                        
-                        # listac.pop(fila,column)
-
-                        #----  listac.insertar(fila,column,1)  
-        #------------------------------------------------------------------------------------------------------------------
+                    
 
             pacientep.print()
-            numeropaciente=input("ingrese el paciente que desea examinar: ")
-            
-            #npaciente.celdas.buscar("3","0")
-
-
-           # npaciente=pacientep.buscarpaciente("Criscros")
-            #npaciente.celdas.mostrarCeldas()
+            print(" ")
+            numeropaciente=input(Fore.CYAN+"ingrese el paciente que desea examinar: ")
             nuevonumero.limpiarList()
 
             busqueda1=pacientep.returnNombrePaciente(int(numeropaciente))
@@ -114,26 +94,190 @@ def menu():
             print(" ")
             diagnostico=npaciente.celdas.enfermedad(periodos)
             if diagnostico == 0:
-                print("\n Diagnostico: La enfermedad es leve")
+                print(Fore.BLUE+"\n Diagnostico: La enfermedad es leve")
             elif diagnostico == 1:
-                print("\n Diagnostico: La enfermedad es mortal")
+                print(Fore.LIGHTRED_EX+"\n Diagnostico: La enfermedad es mortal")
             elif diagnostico > 1:
-                print("\n Diagnostico: La enfermedad es grave")
+                print(Fore.RED+"\nDiagnostico: La enfermedad es grave")
             else:
                 pass
             print(" ")
             print(" ")
             generarPDF(periodos,busqueda1)
-           # print("")
-          #  print("-----------------------------"
-         #   npaciente.celdas.mostrarCeldas()
-            #npaciente.celdas.generarpdf()
-            #npaciente.celdas.graficar()
+
             nuevonumero.limpiarList()
         elif n=="3":
+            nuevonumero=ListadoNumeral()
+            tree=ET.parse(ruta)
+            root=tree.getroot()
+            pacientep.limpiarList()
+            pacientep=passiente()
+            for paciente in root:
+                for datospersonales in paciente:
+                    if datospersonales.tag.lower()=="datospersonales":
+                        for dato in datospersonales:
+                            if dato.tag.lower() =="nombre":
+                                namep=str(dato.text)
+                            if dato.tag.lower() =="edad":
+                                edadp=str(dato.text)
+
+                    if datospersonales.tag.lower() =="periodos":
+                        periodosp=str(datospersonales.text)
+                    if datospersonales.tag.lower() =="m":
+                        m=int(datospersonales.text)+1
+                        mp=str(datospersonales.text)
+                        neww=pacienten(namep,edadp,periodosp,mp)
+                        pacientep.append(neww)
+                        npaciente=pacientep.buscarpaciente(namep)
+
+                    if datospersonales.tag.lower() =="rejilla":
+                        for rejilla in datospersonales:
+
+                            fila=int(rejilla.attrib["f"])
+                            column=int(rejilla.attrib["c"])
+                            nuevasceldas=Celda(fila,column,1)
+                            npaciente.celdas.append(nuevasceldas)
+                        i=1
+                        for x in range(i, m):
+                            j=1
+                            for y in  range(j, m):
+                                npaciente.celdas.buscar(int(x),int(y))
+                    
+
+            pacientep.print()
+            print(" ")
+            numeropaciente=input(Fore.CYAN+"ingrese el paciente que desea examinar: ")
+            nuevonumero.limpiarList()
+
+            busqueda1=pacientep.returnNombrePaciente(int(numeropaciente))
+            npaciente=pacientep.buscarpaciente(str(busqueda1))
+            periodos=10000
+            npaciente.celdas.evaluar(periodos)
+            print(" ")
+            print(" ")
+            diagnostico=npaciente.celdas.enfermedad(periodos)
+            if diagnostico == 0:
+                print(Fore.BLUE+"\n Diagnostico: La enfermedad es leve")
+            elif diagnostico == 1:
+                print(Fore.LIGHTRED_EX+"\n Diagnostico: La enfermedad es mortal")
+            elif diagnostico > 1:
+                print(Fore.RED+"\nDiagnostico: La enfermedad es grave")
+            else:
+                pass
+            print(" ")
+            print(" ")
+            generarPDF(periodos,busqueda1)
+
+            nuevonumero.limpiarList()
+        elif n=="4":
+            nuevonumero=ListadoNumeral()
+            tree=ET.parse(ruta)
+            root=tree.getroot()
+            pacientep.limpiarList()
+            pacientep=passiente()
+            for paciente in root:
+                for datospersonales in paciente:
+                    if datospersonales.tag.lower()=="datospersonales":
+                        for dato in datospersonales:
+                            if dato.tag.lower() =="nombre":
+                                namep=str(dato.text)
+                            if dato.tag.lower() =="edad":
+                                edadp=str(dato.text)
+
+                    if datospersonales.tag.lower() =="periodos":
+                        periodosp=str(datospersonales.text)
+                    if datospersonales.tag.lower() =="m":
+                        m=int(datospersonales.text)+1
+                        mp=str(datospersonales.text)
+                        neww=pacienten(namep,edadp,periodosp,mp)
+                        pacientep.append(neww)
+                        npaciente=pacientep.buscarpaciente(namep)
+
+                    if datospersonales.tag.lower() =="rejilla":
+                        for rejilla in datospersonales:
+
+                            fila=int(rejilla.attrib["f"])
+                            column=int(rejilla.attrib["c"])
+                            nuevasceldas=Celda(fila,column,1)
+                            npaciente.celdas.append(nuevasceldas)
+                        i=1
+                        for x in range(i, m):
+                            j=1
+                            for y in  range(j, m):
+                                npaciente.celdas.buscar(int(x),int(y))
+
+            imprimir=int(pacientep.returnTamano())
+            pacientes=minidom.Document()
+            xml=pacientes.createElement("pacientes")
+            pacientes.appendChild(xml)
+
+
+            for num in range(1,imprimir+1):
+                nuevonumero.limpiarList()
+
+                busqueda1=pacientep.returnNombrePaciente(int(num))
+                npaciente=pacientep.buscarpaciente(str(busqueda1))
+                periodos=int(npaciente.periodo)
+                npaciente.celdas.evaluarXML(periodos)
+                patronenfermo=npaciente.celdas.RetornarPatron(periodos)
+                diagnostico=npaciente.celdas.enfermedadXML(periodos)
+          #      patronenfermo=npaciente.celdas.RetornarCeldaEnferma(periodos)
+                if diagnostico == 0:
+                    resultado="leve"
+                elif diagnostico == 1:
+                    resultado="mortal"
+                elif diagnostico > 1:
+                    resultado="grave"
+                else:
+                    pass     
+                pacientexml=pacientes.createElement("paciente")
+                xml.appendChild(pacientexml)
+                datospersonalesxml=pacientes.createElement("datospersonales")
+                pacientexml.appendChild(datospersonalesxml)
+
+                datoxml=pacientes.createElement("nombre")
+                datoxml.appendChild(pacientes.createTextNode("{}".format(npaciente.nombre)))
+                datospersonalesxml.appendChild(datoxml)
+            
+                datoxml=pacientes.createElement("edad")
+                datoxml.appendChild(pacientes.createTextNode("{}".format(npaciente.edad)))
+                datospersonalesxml.appendChild(datoxml)
+
+                periodosxml=pacientes.createElement("periodos")
+                periodosxml.appendChild(pacientes.createTextNode("{}".format(npaciente.periodo)))
+                pacientexml.appendChild(periodosxml)
+
+                mxml=pacientes.createElement("m")
+                mxml.appendChild(pacientes.createTextNode("{}".format(npaciente.m)))
+                pacientexml.appendChild(mxml)
+
+                resultadoxml=pacientes.createElement("m")
+                resultadoxml.appendChild(pacientes.createTextNode("{}".format(resultado)))
+                pacientexml.appendChild(resultadoxml)
+
+                Nxml=pacientes.createElement("N")
+                Nxml.appendChild(pacientes.createTextNode("{}".format(patronenfermo)))
+                pacientexml.appendChild(Nxml)
+
+                N1xml=pacientes.createElement("N1")
+                N1xml.appendChild(pacientes.createTextNode("{}".format(diagnostico)))
+                pacientexml.appendChild(N1xml)
+
+                nuevonumero.limpiarList()
+
+           
+
+            xml_str=pacientes.toprettyxml(indent="\t")
+            save_path_file="Reporte_{}.xml".format(numeroxml)
+            with open(save_path_file,"w") as f:
+                f.write(xml_str) 
+
+            numeroxml=numeroxml+1
+        elif n=="5":
             return False
         else:
             pass
+
 def generarPDF(periodos,nombre):
     pdf=FPDF(orientation="P",unit="mm",format="A4")
     for i in range(0,(periodos+1)):
